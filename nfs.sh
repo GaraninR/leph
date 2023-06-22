3ef00899b22d635d9cbc0ba2437ca897e5c934e2
Roman GaraninR <garanin.r@gmail.com>
2023-06-22 21:13:24 +0000

    Nfs fixes

--- nfs.sh
+++ nfs.sh
@@ -3,3 +3,26 @@
 echo ""========== NFS "=========="
 export DEBIAN_FRONTEND=noninteractive
 sudo apt install nfs-kernel-server nfs-common -y
+
+number_of_nfs_volumes=30
+nfs_dir_path="/home/vagrant/Storages"
+dir_prefix="volume"
+
+mkdir -p $nfs_dir_path
+
+for (( i=1;i<=$number_of_nfs_volumes;++i ))
+do
+    if [ $i -lt 10 ]; then
+        dirname="$nfs_dir_path/${dir_prefix}0${i}"
+    else
+        dirname="$nfs_dir_path/$dir_prefix$i"
+    fi
+
+    if [ ! -f "first_run" ]; then
+        mkdir -p $dirname
+        echo "$dirname *(rw,sync,fsid=0,crossmnt,no_subtree_check)" | sudo tee -a /etc/exports > /dev/null
+    fi
+    
+done
+
+sudo exportfs -ar
