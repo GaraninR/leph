5d0f6b45d7f5a8d8311d585e3597ecd00b4f6b28
Roman GaraninR <garanin.r@gmail.com>
2023-06-22 20:20:04 +0000

    More aliases and nodejs fixes

--- init.sh
+++ init.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 chmod +x $PWD/*.sh
-mkdir ~/Projects && mkdir ~/Infrastructure
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update -y && apt upgrade -y
 sudo apt install neovim git mc bash-completion curl \
     wget apt-transport-https software-properties-common -y
+
+if [ ! -f "first_run" ]; then
+    touch 'first_run'
+    mkdir ~/Projects && mkdir ~/Infrastructure
+    echo 'alias vim=nvim' >>~/.bashrc
+    echo 'alias vim=nvim' >>~/.profile
+fi
+
